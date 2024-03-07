b39ffc35e88e4854583985a37ab458d03179b136
Juanvi1905 <146494823+Juanvi1905@users.noreply.github.com>
2024-03-07 18:30:36 -0500

    Add files via upload

---
+++ trabajoempleados.py
@@ -0,0 +1,23 @@
+class Empleado:
+    def __init__(self):
+        self.nombre = input("Ingrese el nombre del empleado: ")
+
+
+class Salario:
+    def __init__(self):
+        self.salario = float(input("Ingrese el salario del empleado: "))
+
+
+class Designacion(Empleado, Salario):
+    def __init__(self):
+        Empleado.__init__(self)
+        Salario.__init__(self)
+
+    def designar_cargo(self, cargo):
+        self.cargo = cargo
+
+
+
+objeto_designacion = Designacion()
+print("¿El objeto tiene el método 'nombre'? ", hasattr(objeto_designacion, 'nombre'))
+print("¿El objeto tiene el método 'salario'? ", hasattr(objeto_designacion, 'salario'))
